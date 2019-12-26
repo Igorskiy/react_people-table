23224f19a8816768b8dfc902fa717b972296e7a3
Ihor Stavratii <igor.stavratiy@gmail.com>
2019-12-26 13:57:26 +0200

    Updates

---
+++ precache-manifest.d5bad3baa69d57f50c9a9f7db8860529.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf1ffd448acb57512ca2d5eea97218e5",
+    "url": "/react_people-table/index.html"
+  },
+  {
+    "revision": "afb0a205e97f164b1c61",
+    "url": "/react_people-table/static/css/main.e24fff49.chunk.css"
+  },
+  {
+    "revision": "e6ee391842550dacb4d4",
+    "url": "/react_people-table/static/js/2.e5400377.chunk.js"
+  },
+  {
+    "revision": "afb0a205e97f164b1c61",
+    "url": "/react_people-table/static/js/main.9e221b75.chunk.js"
+  },
+  {
+    "revision": "c6b2e506936d419d6d52",
+    "url": "/react_people-table/static/js/runtime~main.b2b73302.js"
+  }
+]);
